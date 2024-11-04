c3f46b574058773d80e4801d5f580fc821e8c9fa
Divya Navale <navad01@pfw.edu>
2024-11-04 17:19:20 -0500

    removed useLocation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Register from './pages/signUp'
 import ForgotPassword from './pages/forgotPassword'
 import './App.css';
 import './../src/styles/styles.css'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 
 function App() {
